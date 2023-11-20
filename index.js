95e30ba4fcb50f2ae62372fe6b9421bbbec52477
Jihad Hasan <mdjihat0021@gmail.com>
2023-11-20 20:31:58 +0600

    vercel

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 // middilewares
-app.use(cors())
+app.use(cors({
+  origin: [
+      'http://localhost:5173',
+      
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 
